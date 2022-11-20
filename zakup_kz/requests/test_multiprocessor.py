7a99333cef57e4d6c17f9f9671e48142dd4255d8
Vadim Gubergrits <vadim.gubergrits@gmail.com>
2022-11-20 13:57:19 -0500

    Add configuration for exception and error backoff.

---
+++ zakup_kz/requests/test_multiprocessor.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest import mock
+
+import responses
+
+import multiprocessor
+
+
+responses.get("https://ipv4.webshare.io/", "0.0.0.0")
+
+
+class MultiprocessorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.processor = multiprocessor.MultiProcessor(
+            config_path="config.json",
+            delay502=10,
+            delay502_increment=1
+        )
+
+    @responses.activate 
+    def test_delay(self):
+        # Make sure that we call multi_monitor with the proper delay parameters.
+        self.assertEqual(self.processor.delay502, 10)
+        self.assertEqual(self.processor.delay502_increment, 1)
+
+        with mock.patch("auth.AuthClass.run_auth") as run_auth:
+            run_auth.return_value = False
+            with mock.patch("monitor.Monitor.multi_monitor") as multi_monitor:
+                multi_monitor.return_value = None
+                self.processor.run()
+                multi_monitor.assert_called_once_with(
+                    delay_502=self.processor.delay502,
+                    delay_502_increment=self.processor.delay502_increment
+                )
+
+    def test_stats(self):
+        self.processor.order = ""
+        self.processor.auth_end_time, self.processor.auth_start_time = 10, 0
+        self.processor.create_end_time, self.processor.create_start_time = 20, 10
+        self.processor.documents_end_time, self.processor.documents_start_time = 30, 20
+        self.processor.affiliate_end_time, self.processor.affiliate_start_time = 40, 30
+        self.processor.prices_end_time, self.processor.prices_start_time = 50, 40
+        stats = self.processor.get_stats()
+        self.assertIn("TOTAL AUTH TIME 10.0", stats)
+        self.assertIn("APPLICATION CREATION 10.0", stats)
+        self.assertIn("DOCUMENTS UPLOAD TIME 10.0", stats)
+        self.assertIn("AFFILIATE WAIT 10.0", stats)
+        self.assertIn("PRICES UPLOAD 10.0", stats)
+
+
+class MultiProcessingMainTestCase(unittest.TestCase):
+    @responses.activate 
+    def test_exception_delay(self):
+        exception_delay = 20
+        args = multiprocessor.parser.parse_args([
+            "--config=config.json",
+            f"--exception-delay={exception_delay}",
+        ])
+        with mock.patch("multiprocessor.MultiProcessor.run") as run:
+            run.side_effect = [KeyError, None]
+            with mock.patch("time.sleep") as sleep:
+                multiprocessor.main(args)
+                sleep.assert_called_once_with(exception_delay)
+
+
+if __name__ == "__main__":
+    unittest.main()
